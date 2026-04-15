469f3409d3fe97ae7f0a7ed7eacd2f028799681e
Gerardo2006 <00043524@uca.edu.sv>
2026-04-14 21:34:54 -0600

    hotfix: Quitando comentacios

--- app/src/main/java/com/example/practicamoviles/MainActivity2.kt
+++ app/src/main/java/com/example/practicamoviles/MainActivity2.kt
@@ -1,4 +1,4 @@
-package com.example.practicamoviles // Cambiado para coincidir con la carpeta
+package com.example.practicamoviles
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,7 +18,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-// ELIMINADO: import androidx.material3.R
+
 
 @Composable
 @Preview
@@ -55,7 +55,6 @@ fun MainScreen() {
                 fontWeight = FontWeight.Bold
             )
 
-            // Asegúrate de que el archivo se llame clima.webp en minúsculas
             Image(
                 painter = painterResource(id = R.drawable.clima),
                 contentDescription = "Icono del clima",
